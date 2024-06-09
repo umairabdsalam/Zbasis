6e3e272b8e8f194e8cb2ec0c0869ce2ecc237e5f
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-06-09 01:41:43 +0100

    Initial Commit

---
+++ ZBasis/ZBasisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZBasisApp.swift
+//  ZBasis
+//
+//  Created by Umair Salam on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ZBasisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
